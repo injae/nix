e43221b1437cee8c9f28bbc987a38b260df0c9f4
injae <8687lee@gmail.com>
2024-07-30 08:30:14 +0000

    update nixos-wsl config

--- systems/nixos-wsl/default.nix
+++ systems/nixos-wsl/default.nix
@@ -1,30 +1,69 @@
-{ pkgs, flake, config, ... }:
+{ pkgs, flake, config, lib, ... }:
 let
   inherit (flake) inputs;
   inherit (inputs) self;
   user = flake.config.people.myself;
+  home-dir = "/home/${user}";
 in
 {
   imports = [
     # https://nix-community.github.io/NixOS-WSL/options.html
     inputs.nixos-wsl.nixosModules.default
     self.nixosModules.default
+    inputs.sops-nix.nixosModules.sops
   ];
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "24.05";
-  wsl.enable = true;
-  wsl.defaultUser = user;
-  wsl.startMenuLaunchers = true;
+  wsl = {
+    enable = true;
+    defaultUser = user;
+    startMenuLaunchers = true;
+    wslConf.interop.appendWindowsPath = false;
+  };
 
   boot.tmp.cleanOnBoot = true;
 
+  # docker setting
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
   programs.zsh.enable = true;
+  #programs.nix-ld.enable = true;
+  #programs.nix-ld.libraries = with pkgs; [
+  #  bazelisk
+  #];
 
   environment.systemPackages = with pkgs; [
+    gccStdenv
     emacs-pgtk
     wslu
+    xdg-utils
   ];
-  services.emacs.enable = true;
+  environment.variables.JAVAX_NET_SSL_TRUSTSTORE =
+    let
+      caBundle = config.environment.etc."ssl/certs/ca-bundle.crt".source;
+      p11kit = pkgs.p11-kit.overrideAttrs (oldAttrs: {
+        configureFlags = [
+          "--with-trust-paths=${caBundle}"
+        ];
+      });
+    in
+    derivation {
+      name = "java-cacerts";
+      builder = pkgs.writeShellScript "java-cacerts-builder" ''
+        ${p11kit.bin}/bin/trust \
+          extract \
+          --format=java-cacerts \
+          --purpose=server-auth \
+          $out
+      '';
+      system = pkgs.system;
+    };
+  environment.variables.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  #services.emacs.enable = true;
 
   users.users.${user} = {
     isNormalUser = true;
@@ -32,5 +71,10 @@ in
     extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
 
-  security.sudo.enable = true;
+  security = {
+    sudo.enable = true;
+    pki.certificateFiles = [
+    ];
+  };
+
 }
