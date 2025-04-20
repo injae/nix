0ab9dcc137bd03bcae80793dcd7f0dee631a15f1
injae <8687lee@gmail.com>
2025-04-20 14:25:37 +0900

    disable macos ollama

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -50,21 +50,21 @@ in
 
   # enable ollama service
   # https://github.com/LnL7/nix-darwin/pull/972/files
-  environment.systemPackages = [ pkgs.ollama ];
-  launchd.user.agents.ollama = {
-    path = [ config.environment.systemPath ];
-    serviceConfig = {
-      KeepAlive = true;
-      RunAtLoad = true;
-      ProgramArguments = [
-        "${pkgs.ollama}/bin/ollama"
-        "serve"
-      ];
-      EnvironmentVariables = {
-        OLLAMA_HOST = "127.0.0.1:11434";
-      };
-    };
-  };
+  #environment.systemPackages = [ pkgs.ollama ];
+  #launchd.user.agents.ollama = {
+  #  path = [ config.environment.systemPath ];
+  #  serviceConfig = {
+  #    KeepAlive = true;
+  #    RunAtLoad = true;
+  #    ProgramArguments = [
+  #      "${pkgs.ollama}/bin/ollama"
+  #      "serve"
+  #    ];
+  #    EnvironmentVariables = {
+  #      OLLAMA_HOST = "127.0.0.1:11434";
+  #    };
+  #  };
+  #};
 
   # Enable touch id for sudo
   security.pam.services.sudo_local.touchIdAuth = true;
