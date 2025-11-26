afa65c4874921bbf8b4a9d65d84138823f165fbd
injae <8687lee@gmail.com>
2025-11-26 19:14:22 +0900

    remove "aarch64-linux"

--- flake.nix
+++ flake.nix
@@ -66,7 +66,6 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
