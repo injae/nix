a67e119bfbdd3efad1aa1333fd360b950c5fd961
injae <8687lee@gmail.com>
2026-02-27 12:17:54 +0900

    fix go template

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -4,8 +4,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -14,34 +16,39 @@
       in
       {
         packages = rec {
-          example = with pkgs; buildGoModule {
-            pname = "example";
-            version = "0.1.0";
-            src = lib.cleanSource ./.;
-            subPackages = [ "cmd/example" ];
-            vendorHash = lib.fakeHash;
-            NativeBuildInputs = [ ];
-            meta = with lib; {
-              description = "";
-              homepage = "";
-              license = licenses.mit;
-              maintainers = with maintainers; [ injae ];
+          example =
+            with pkgs;
+            buildGoModule {
+              pname = "example";
+              version = "0.1.0";
+              src = lib.cleanSource ./.;
+              subPackages = [ "cmd/example" ];
+              vendorHash = lib.fakeHash;
+              NativeBuildInputs = [ ];
+              meta = with lib; {
+                description = "";
+                homepage = "";
+                license = licenses.mit;
+                maintainers = with maintainers; [ injae ];
+              };
             };
-          };
           default = example;
         };
-        devShells.default = with pkgs; mkShell {
-          packages = [
-            nixfmt-rfc-style
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              nixfmt-rfc-style
 
-            go
-            gopls
-            golangci-lint
-            gotools
-            delve
-          ];
-          buildInputs = [ ];
-          shellHook = '''';
-        };
-      });
+              go
+              gopls
+              golangci-lint
+              gotools
+              delve
+            ];
+            buildInputs = [ ];
+            shellHook = '''';
+          };
+      }
+    );
 }
