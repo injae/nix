dc905d761f562c7871c8f08002dcd00f8b2adadc
injae <8687lee@gmail.com>
2026-03-26 09:52:35 +0900

    update elpaca-installer-version to 0.12

--- modules/home/shared/programs/emacs/config/init.el
+++ modules/home/shared/programs/emacs/config/init.el
@@ -31,20 +31,18 @@
 (setq-default user-nix-directory (expand-file-name "~/nix"))
 (setq-default user-mutable-emacs-directory (expand-file-name "modules/home/shared/programs/emacs" user-nix-directory))
 
-(defvar elpaca-installer-version 0.11)
-
 ;; (elpaca-write-lock-file)
 ;;(setq elpaca-lock-file (expand-file-name "elpaca-lock.el" user-mutable-emacs-directory))
+(defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
-
        (order (cdr elpaca-order))
        (default-directory repo))
   (add-to-list 'load-path (if (file-exists-p build) build repo))
@@ -76,7 +74,7 @@
 
 (elpaca elpaca-use-package
     (elpaca-use-package-mode)
-    (setq elpaca-use-package-by-default t)
+    (setq use-package-always-ensure t)
     )
 
 (elpaca-wait)
