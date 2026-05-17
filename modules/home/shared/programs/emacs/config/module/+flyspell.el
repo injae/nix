533d624145c6820683158ea42271fea500fbe52e
injae <8687lee@gmail.com>
2026-05-17 16:58:07 +0900

    separate flyspell module from flycheck module

---
+++ modules/home/shared/programs/emacs/config/module/+flyspell.el
@@ -0,0 +1,45 @@
+;;; +flyspell.el --- Summery -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package flyspell :ensure nil
+    :general (leader "sk" '((lambda () (interactive) (ispell-change-dictionary "ko_KR") (flyspell-buffer)) :wk "Spell Dictionary Korean")
+                     "se" '((lambda () (interactive) (ispell-change-dictionary "en_US") (flyspell-buffer)) :wk "Spell Dictionary English"))
+    :hook ((prog-mode . flyspell-mode)
+           (text-mode . flyspell-mode))
+    :custom (ispell-dictionary   "en_US")
+    :config
+        ;; Make sure new aspell is installed
+        (when (executable-find "aspell")
+            (setq ispell-program-name "aspell")
+            (setq ispell-list-command "--list"))
+        (setq-default ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--camel-case"))
+
+        (setq ispell-personal-dictionary (f-join user-mutable-emacs-directory "config/.personal-dict"))
+        ;; 스펠체크 넘어가는 부분 설정
+        ;(add-to-list 'ispell-skip-region-alist '(":\\(PROPERTIES\\|LOGBOOK\\):" . ":END:"))
+        ;(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_SRC" . "#\\+END_SRC"))
+        ;(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_EXAMPLE" . "#\\+END_EXAMPLE"))
+)
+
+(use-package flyspell-correct :after flyspell
+    :general (leader "sf"  #'flyspell-correct-wrapper)
+    :config
+    (define-key flyspell-mode-map (kbd "C-;") #'flyspell-correct-wrapper)
+    )
+
+(use-package consult-flyspell :after (flyspell-correct consult)
+    :custom
+          (consult-flyspell-select-function      nil
+           consult-flyspell-set-point-after-word t
+           consult-flyspell-always-check-buffer  nil)
+          (consult-flyspell-correct-function (lambda () (flyspell-correct-at-point) (consult-flyspell))))
+
+(use-package jinx :disabled
+    ;; https://github.com/spellcheck-ko/hunspell-dict-ko
+    :hook (emacs-startup . global-jinx-mode)
+    :general (leader "sf" #'jinx-correct)
+    )
+
+(provide '+flyspell)
+;;; +flyspell.el ends here
