389ab1cd258150072dca90f3bd986a490e8c1b0a
injae <8687lee@gmail.com>
2026-05-17 19:02:39 +0900

    fix eglot rassumfrassum nix config

--- modules/home/shared/programs/emacs/rassumfrassum/nix.py
+++ modules/home/shared/programs/emacs/rassumfrassum/nix.py
@@ -1,3 +1,5 @@
+import socket
+
 from rassumfrassum.frassum import LspLogic, Server, DirectResponse
 from rassumfrassum.json import JSON
 from rassumfrassum.util import dmerge
@@ -5,35 +7,41 @@ from rassumfrassum.util import dmerge
 initialize_method = "initialize"
 initialization_options = "initializationOptions"
 
+
 def get_nix_path(params: JSON) -> str:
-    #return params.get("rootPath")
-    return "/Users/nieel/nix"
+    return params.get("rootPath").rstrip("/")
+
 
 def get_nix_host() -> str:
-    return "nieel-m3"
+    return socket.gethostname().removesuffix(".local")
+
 
 class NixLogic(LspLogic):
-    async def on_client_request(self, method: str, params: JSON, servers: list[Server]) -> list[Server] | DirectResponse:
+    async def on_client_request(
+        self, method: str, params: JSON, servers: list[Server]
+    ) -> list[Server] | DirectResponse:
         if method == initialize_method:
             root_path = get_nix_path(params)
-            params[initialization_options] = dmerge(params.get(initialization_options, {}), {
-                "nixpkgs": {"expr": "import <nixpkgs> { }"},
-                "formatting": {"command": ["nixfmt"]},
-                "options": {
-                    "nixos": {
-                        "expr": f'(builtins.getFlake "{root_path}").darwinConfigurations.{get_nix_host()}.options',
-                    },
-                    "home-manager": {
-                        "expr": f'(builtins.getFlake "{root_path}").homeConfigurations.{get_nix_host()}.options',
+            params[initialization_options] = dmerge(
+                params.get(initialization_options, {}),
+                {
+                    "nixpkgs": {"expr": "import <nixpkgs> { }"},
+                    "formatting": {"command": ["nixfmt"]},
+                    "options": {
+                        "nixos": {
+                            "expr": f'(builtins.getFlake "{root_path}").darwinConfigurations.{get_nix_host()}.options',
+                        },
+                        "home-manager": {
+                            "expr": f'(builtins.getFlake "{root_path}").homeConfigurations.{get_nix_host()}.options',
+                        },
                     },
                 },
-            })
+            )
         return await super().on_client_request(method, params, servers)
 
+
 def servers():
-    return [
-        ['nixd']
-    ]
+    return [["nixd"]]
 
 
 def logic_class():
