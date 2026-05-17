195c809899ebf6034c71e4226b9c17064289027a
injae <8687lee@gmail.com>
2026-05-17 17:02:19 +0900

    add modules and formatting

--- modules/home/shared/programs/emacs/config/init.el
+++ modules/home/shared/programs/emacs/config/init.el
@@ -4,25 +4,25 @@
 ;;; Code:
 
 ;; Waring: Unable to determine elpaca-core-date
-;;get number (string-to-number (nth 1 (elpaca-process-call "git" "log" "-n" "1" "--format=%cd" "--date=format:%Y%m%d")))
+;; get number (string-to-number (nth 1 (elpaca-process-call "git" "log" "-n" "1" "--format=%cd" "--date=format:%Y%m%d")))
 ;; https://github.com/progfolio/elpaca/issues/222
 
-;(defun my/nixos/get-emacs-build-date ()
-;  "Return NixOS Emacs build date."
-; (string-match "--prefix.*emacs.*\\([[:digit:]]\\{8\\}\\)" system-configuration-options)
-;  (string-to-number (match-string 1 system-configuration-options)))
+;; (defun my/nixos/get-emacs-build-date ()
+;;   "Return NixOS Emacs build date."
+;;  (string-match "--prefix.*emacs.*\\([[:digit:]]\\{8\\}\\)" system-configuration-options)
+;;   (string-to-number (match-string 1 system-configuration-options)))
 
 ;; https://github.com/progfolio/elpaca/issues/222
 ;;(when (not (boundp 'elpaca-core-date)) (setq elpaca-core-date (list (my/nixos/get-emacs-build-date))))
 
 (defun my/nixos-p ()
-  "Return t if operating system is NixOS, nil otherwise."
-  (string-match-p "NixOS" (shell-command-to-string "uname -v")))
+    "Return t if operating system is NixOS, nil otherwise."
+    (string-match-p "NixOS" (shell-command-to-string "uname -v")))
 
 (defun my/nixos/get-emacs-build-date ()
-  "Return NixOS Emacs build date."
-  (string-match "--prefix.*emacs.*\\([[:digit:]]\\{8\\}\\)" system-configuration-options)
-  (string-to-number (match-string 1 system-configuration-options)))
+    "Return NixOS Emacs build date."
+    (string-match "--prefix.*emacs.*\\([[:digit:]]\\{8\\}\\)" system-configuration-options)
+    (string-to-number (match-string 1 system-configuration-options)))
 
 ;; Run this before the elpaca.el is loaded. Before the installer in your init.el is a good spot.
 (when (my/nixos-p) (setq elpaca-core-date (list (my/nixos/get-emacs-build-date))))
@@ -38,37 +38,37 @@
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1 :inherit ignore
-                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca-activate)))
+                          :ref nil :depth 1 :inherit ignore
+                          :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                          :build (:not elpaca-activate)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (<= emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-                                                  ,@(when-let* ((depth (plist-get order :depth)))
-                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
-                                                  ,(plist-get order :repo) ,repo))))
-                  ((zerop (call-process "git" nil buffer t "checkout"
-                                        (or (plist-get order :ref) "--"))))
-                  (emacs (concat invocation-directory invocation-name))
-                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                  ((require 'elpaca))
-                  ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+          (build (expand-file-name "elpaca/" elpaca-builds-directory))
+          (order (cdr elpaca-order))
+          (default-directory repo))
+    (add-to-list 'load-path (if (file-exists-p build) build repo))
+    (unless (file-exists-p repo)
+        (make-directory repo t)
+        (when (<= emacs-major-version 28) (require 'subr-x))
+        (condition-case-unless-debug err
+            (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                         ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                            ,@(when-let* ((depth (plist-get order :depth)))
+                                                                  (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                            ,(plist-get order :repo) ,repo))))
+                         ((zerop (call-process "git" nil buffer t "checkout"
+                                     (or (plist-get order :ref) "--"))))
+                         (emacs (concat invocation-directory invocation-name))
+                         ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                     "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                         ((require 'elpaca))
+                         ((elpaca-generate-autoloads "elpaca" repo)))
+                (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+                (error "%s" (with-current-buffer buffer (buffer-string))))
+            ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+    (unless (require 'elpaca-autoloads nil t)
+        (require 'elpaca)
+        (elpaca-generate-autoloads "elpaca" repo)
+        (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
@@ -85,16 +85,16 @@
 (use-package exec-path-from-shell
     :custom (
                 (exec-path-from-shell-variables '("PATH"
-                                                  "MANPATH"
-                                                  "TMPDIR"
-                                                  "KUBECONFIG"
-                                                  "LSP_USE_PLISTS"
-                                                  "GOPRIVATE"
-                                                  "RUST_BACKTRACE"
-                                                  "MallocNanoZone"
-                                                  "NIX_SSL_CERT_FILE"
-                                                  "SSL_CERT_FILE"
-                                                  "REQUEST_CA_BUNDLE"))
+                                                     "MANPATH"
+                                                     "TMPDIR"
+                                                     "KUBECONFIG"
+                                                     "LSP_USE_PLISTS"
+                                                     "GOPRIVATE"
+                                                     "RUST_BACKTRACE"
+                                                     "MallocNanoZone"
+                                                     "NIX_SSL_CERT_FILE"
+                                                     "SSL_CERT_FILE"
+                                                     "REQUEST_CA_BUNDLE"))
                 (exec-path-from-shell-arguments '("-l"))
                 (exec-path-from-shell-check-startup-files nil)
                 (exec-path-from-shell-debug nil)
@@ -159,7 +159,7 @@
              project-manage completion
              window buffer ui
              org terminal edit
-             flycheck search
+             flycheck flyspell search
              multi-mode util
              run-command
              third-party
@@ -177,8 +177,9 @@
              flutter web ruby
              jvm  go  nix lua
              config-file docker
+             yaml toml json
              formatting bazel
-             markdown json terraform
+             markdown terraform
              ))
     )
 
