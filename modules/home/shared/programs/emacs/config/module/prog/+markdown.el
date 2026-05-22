697da2469a1c2f7924ad3c0ba24aebe60dc26c80
injae <8687lee@gmail.com>
2026-05-22 16:46:49 +0900

    refactor(emacs/markdown): centralize treesit grammar config in +tree-sitter.el

--- modules/home/shared/programs/emacs/config/module/prog/+markdown.el
+++ modules/home/shared/programs/emacs/config/module/prog/+markdown.el
@@ -2,14 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package markdown-mode)
+
+
 (use-package markdown-ts-mode
-    :mode ("\\.md\\'"       . markdown-ts-mode)
-    :config
-    (add-to-list 'treesit-language-source-alist
-        '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
-    (add-to-list 'treesit-language-source-alist
-        '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src"))
-    )
+    :mode ("\\.md\\'" . markdown-ts-mode)
+    :hook (markdown-ts-mode . my/eglot-ensure))
 
 (provide '+markdown)
 ;;; +markdown.el ends here
