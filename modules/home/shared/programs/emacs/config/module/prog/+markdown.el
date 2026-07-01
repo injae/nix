cf26cbf53368d3bef7a020b8a015c3e79fcdfd9e
injae <8687lee@gmail.com>
2026-07-01 13:22:45 +0900

    feat(emacs/markdown): use builtin markdown-ts-mode and tidy tree-sitter grammars

--- modules/home/shared/programs/emacs/config/module/prog/+markdown.el
+++ modules/home/shared/programs/emacs/config/module/prog/+markdown.el
@@ -4,10 +4,11 @@
 
 (use-package markdown-mode)
 
-
-(use-package markdown-ts-mode
+(use-package markdown-ts-mode :ensure nil
     :mode ("\\.md\\'" . markdown-ts-mode)
-    :hook (markdown-ts-mode . my/eglot-ensure))
+    :hook (markdown-ts-mode . my/eglot-ensure)
+    :custom (eglot-documentation-renderer 'markdown-ts-view-mode)
+    )
 
 (provide '+markdown)
 ;;; +markdown.el ends here
