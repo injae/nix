8b84887ae79609a2b61710184e54deba0cee89f7
injae <8687lee@gmail.com>
2026-07-01 13:21:16 +0900

    fix(emacs/doc): hide eldoc-box childframe on window/buffer change

--- modules/home/shared/programs/emacs/config/module/prog/+doc.el
+++ modules/home/shared/programs/emacs/config/module/prog/+doc.el
@@ -6,11 +6,19 @@
   :hook (emacs-startup . global-eldoc-mode))
 
 (use-package eldoc-box
+  :preface
+  ;; hover-mode의 공유 childframe은 글로벌 타이머로만 정리돼 버퍼/윈도우
+  ;; 전환 시 이전 버퍼 내용이 남는다. 전환 시점에 명시적으로 숨긴다.
+  (defun +eldoc-box-quit-on-window-change (&rest _)
+    (eldoc-box-quit-frame))
   :hook (eldoc-mode . eldoc-box-hover-mode)
   :custom
   (eldoc-box-max-pixel-width 600)
   (eldoc-box-max-pixel-height 400)
-  (eldoc-box-clear-with-C-g t))
+  (eldoc-box-clear-with-C-g t)
+  :config
+  (add-hook 'window-selection-change-functions #'+eldoc-box-quit-on-window-change)
+  (add-hook 'window-buffer-change-functions #'+eldoc-box-quit-on-window-change))
 
 
 (provide '+doc)
