d85a54f3e7db97c3ca5d7142efc374e195d7bb5e
injae <8687lee@gmail.com>
2026-07-27 17:35:50 +0900

    expand-region -> expreg

--- modules/home/shared/programs/emacs/config/module/+edit.el
+++ modules/home/shared/programs/emacs/config/module/+edit.el
@@ -37,8 +37,8 @@
 :config (setq show-paren-delay 0)
 )
 
-(use-package expand-region
-:general (leader "tw" '(er/expand-region :wk "Text Wrap"))
+(use-package expreg
+:general (leader "tw" '(expreg-expand :wk "Text Wrap"))
 )
 
 (use-package smartparens
